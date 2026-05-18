98d507ecf4b96c81d3b98afcd01ca3470e2616fb
NonnieChan <131771081+NonnieChan@users.noreply.github.com>
2026-05-18 21:49:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
 const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('SnowBallry Bot is running!');
+});
+
+app.listen(PORT, () => {
+	console.log(`Web server running on port ${PORT}`);
+});
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds]
