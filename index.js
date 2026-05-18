3c1e86b35c47b5044c1af2d577fc1b044ce2360e
NonnieChan <131771081+NonnieChan@users.noreply.github.com>
2026-05-18 21:57:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname)));
+
 app.get('/', (req, res) => {
-	res.send('SnowBallry Bot is running!');
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
