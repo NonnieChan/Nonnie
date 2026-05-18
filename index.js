4614a0af25bd22001fe4ffd2c872538f10dadf41
NonnieChan <131771081+NonnieChan@users.noreply.github.com>
2026-05-18 20:47:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds]
@@ -6,6 +6,16 @@ const client = new Client({
 
 client.once('ready', () => {
 	console.log(`${client.user.tag} is online!`);
+
+	client.user.setPresence({
+		activities: [
+			{
+				name: 'Project Zomboid 🖤',
+				type: ActivityType.Playing
+			}
+		],
+		status: 'idle'
+	});
 });
 
 client.login(process.env.TOKEN);
